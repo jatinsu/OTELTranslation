c2721331a6b6b2fe8cfe33a80237342b2a434173
Jatin Suri <surijatin@yahoo.com>
2023-06-14 11:01:59 -0400

    update

---
+++ yamlWriting/yaml.go
@@ -0,0 +1,27 @@
+package main
+
+import(
+	"fmt"
+	"gopkg.in/yaml.v2"
+)
+
+type Student struct {
+    Name string
+    Age  int
+}
+
+func main() {
+    s1 := Student{
+        Name: "Sagar",
+        Age:  23,
+    }
+
+    yamlData, err := yaml.Marshal(&s1)
+
+    if err != nil {
+        fmt.Printf("Error while Marshaling. %v", err)
+    }
+
+    fmt.Println(" --- YAML ---")
+    fmt.Println(string(yamlData))  // yamlData will be in bytes. So converting it to string.
+}
