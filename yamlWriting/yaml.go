4f1c767f612fe37a855006a7b0c587947d5cc193
Jatin Suri <surijatin@yahoo.com>
2023-06-14 11:40:14 -0400

    update

--- yamlWriting/yaml.go
+++ yamlWriting/yaml.go
@@ -1,27 +1,27 @@
 package main
 
-import(
+import (
 	"fmt"
 	"gopkg.in/yaml.v2"
 )
 
 type Student struct {
-    Name string
-    Age  int
+	Name string
+	Age  int
 }
 
 func main() {
-    s1 := Student{
-        Name: "Sagar",
-        Age:  23,
-    }
+	s1 := Student{
+		Name: "Sagar",
+		Age:  23,
+	}
 
-    yamlData, err := yaml.Marshal(&s1)
+	yamlData, err := yaml.Marshal(&s1)
 
-    if err != nil {
-        fmt.Printf("Error while Marshaling. %v", err)
-    }
+	if err != nil {
+		fmt.Printf("Error while Marshaling. %v", err)
+	}
 
-    fmt.Println(" --- YAML ---")
-    fmt.Println(string(yamlData))  // yamlData will be in bytes. So converting it to string.
+	fmt.Println(" --- YAML ---")
+	fmt.Println(string(yamlData)) // yamlData will be in bytes. So converting it to string.
 }
